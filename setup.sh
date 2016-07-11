f378bb28561a7848e9da7b8ecfeecdea2acb14fd
thiras <antsomers@gmail.com>
2016-07-11 19:32:29 -0400

    Dependency & YouCompleteMe Compile at setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Replace vim-tiny with vim-nox
+apt-get remove vim-tiny
+apt-get update
+apt-get install vim
+
+# Install YouCompleteMe Requirements
+apt-get install build-essential cmake python-dev python3-dev
+
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -7,5 +16,9 @@ cp .vimrc ~
 
 # Vim PluginInstall
 vim +PluginInstall +qall
+
+# YouCompleteMe Compiling
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --tern-completer
 
 echo "Setup Complete"

4e84c745219e669b8ce67f7c4a65fa013ea2579c
thiras <antsomers@gmail.com>
2016-07-12 10:56:58 -0400

    Add OS Detect

--- setup.sh
+++ setup.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+# Distro Detector
+OS=$(lsb_release -si)
+
 # Replace vim-tiny with vim-nox
 apt-get remove vim-tiny
 apt-get update
-apt-get install vim-nox
+
+if [ $OS = "Debian" ]; then
+	apt-get install vim-nox
+else if [ $OS = "Ubuntu" ]; then
+	apt-get install vim-nox-py2
+else
+	echo "Cannot detect OS"
+	break
+fi
+
+# Install NodeJS
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Update NPM
+npm install npm -g
 
 # Install YouCompleteMe Requirements
 apt-get install build-essential cmake python-dev python3-dev
